c750b277c590c4766c60f3e2442b0a7ca0de8eed
sanjeewa <sanjeewaxy@gmail.com>
2024-07-13 08:02:01 +0400

    single book show

--- books/views.py
+++ books/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+import json
+
+booksData=open("E:\\Projects\\python\\django_001\\bookstore\\books\\books.json").read()
+data=json.loads(booksData)
 
 def index(request):
-    return HttpResponse('Book app')
+    context={'data':data}
+    return render(request,'books/index.html',context)
+    # return HttpResponse('Book app')
+    
+def show(request,id):
+    book=list()
+    for d in data:
+        if d['id']==id:
+           book=d 
+
+    context={'book':book}
+    return render(request,'books/single-book.html',context)
