95343d983ce67b24c21cdc0709d1798a25fdaf8d
sanjeewa <sanjeewaxy@gmail.com>
2024-07-15 07:03:00 +0400

    generic details view

--- books/urls.py
+++ books/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns=[
     path('',views.BookListView.as_view(),name='book.all'),
-    path('<int:id>',views.show,name='book.show'),
+    path('<int:pk>',views.BookDetailsView.as_view(),name='book.show'),
     
     path('review',views.review,name='review.add')
 ]
--- books/views.py
+++ books/views.py
@@ -3,7 +3,7 @@ from django.db.models.query import QuerySet
 from django.shortcuts import render,get_object_or_404,redirect
 from books.models import Book,Review
 from django.http import Http404
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 
 class BookListView(ListView):
     def get_queryset(self):
@@ -13,6 +13,9 @@ class BookListView(ListView):
 #     data=Book.objects.all()
 #     context={'data':data}
 #     return render(request,'books/index.html',context)
+    
+class BookDetailsView(DetailView):
+    model=Book
     
 def show(request,id):
     book=get_object_or_404(Book,pk=id)
