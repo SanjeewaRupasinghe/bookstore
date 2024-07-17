908114f3cb782143b2053d184bf8e9620bff1392
sanjeewa <sanjeewaxy@gmail.com>
2024-07-17 06:24:11 +0400

    login page

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -4,4 +4,5 @@ from django.urls import path,include
 urlpatterns = [
     path('books/',include('books.urls')),
     path('admin/', admin.site.urls),
+    path('', include('django.contrib.auth.urls')),
 ]
